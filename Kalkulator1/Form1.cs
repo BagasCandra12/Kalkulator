51a549975aa9c7f47fc25c63d648832902a65730
BagasCandra12 <Bagascandra243@gmail.com>
2021-04-19 10:55:57 +0700

    first commit

---
+++ Kalkulator1/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulator1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            LBLhasil.Text = "";
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("angka 1 dan angka 2 Harus diisi terlebih dahulu");
+            }
+            else
+            {
+
+                int a, b, c;
+                a = int.Parse(this.textBox1.Text);
+                b = int.Parse(this.textBox2.Text);
+                c = a + b;
+                this.LBLhasil.Text = c.ToString();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            LBLhasil.Text = "";
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("angka 1 dan angka 2 Harus diisi terlebih dahulu");
+            }
+            else
+            {
+
+                int a, b, c;
+                a = int.Parse(this.textBox1.Text);
+                b = int.Parse(this.textBox2.Text);
+                c = a - b;
+                this.LBLhasil.Text = c.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("angka 1 dan angka 2 Harus diisi terlebih dahulu");
+            }
+            else
+            {
+
+                int a, b, c;
+                a = int.Parse(this.textBox1.Text);
+                b = int.Parse(this.textBox2.Text);
+                c = a * b;
+                this.LBLhasil.Text = c.ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("angka 1 dan angka 2 Harus diisi terlebih dahulu");
+            }
+            else
+            {
+
+                int a, b, c;
+                a = int.Parse(this.textBox1.Text);
+                b = int.Parse(this.textBox2.Text);
+                c = a / b;
+                this.LBLhasil.Text = c.ToString();
+            }
+        }
+    }
+}
